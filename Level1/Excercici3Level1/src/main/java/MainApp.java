95b8d7cfbcf50a7a94084265e7d6c76518020bae
Jcasas87 <jordicasas87@gmail.com>
2025-10-13 22:25:12 +0200

    Ejercicio 3 level1, terminado

---
+++ Level1/Excercici3Level1/src/main/java/MainApp.java
@@ -0,0 +1,14 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Path ruta = Paths.get("/Users/jcasas87/Desktop/Doc Varios 2025");
+
+        OrderFilesAZWritter.readAddOrderAZWritter(ruta);
+
+    }
+
+}
---
+++ Level1/Excercici3Level1/src/main/java/OrderFilesAZWritter.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class OrderFilesAZWritter {
+
+    static ArrayList<String> lines = new ArrayList<>();
+
+    public static void readAddOrderAZWritter(Path ruta1) {
+
+        Path pathFileWritte = Paths.get("/Users/jcasas87/Desktop/Filelist.txt");
+        ArrayList<Path> firstOrder = new ArrayList<>();
+
+        try (DirectoryStream<Path> content = Files.newDirectoryStream(ruta1)) {
+            for (Path actualFile : content) {
+                firstOrder.add(actualFile);
+            }
+            firstOrder.sort(null);
+
+            for (Path actualFile : firstOrder) {
+                if (Files.isRegularFile(actualFile)) {
+                    String line = actualFile.getFileName() + "--- " + Files.getLastModifiedTime(actualFile) + " <-F->";
+                    lines.add(line);
+
+
+                } else if (Files.isDirectory(actualFile)) {
+                    String line = actualFile.getFileName() + "---" + Files.getLastModifiedTime(actualFile) + " <-D->";
+                    lines.add(line);
+
+                    readAddOrderAZWritter(actualFile);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al leer la carpeta." + e.getMessage());
+        }
+
+        try {
+            Files.write(pathFileWritte, lines);
+        } catch (IOException e) {
+            System.out.println("Error de escritura " + e.getMessage());
+        }
+    }
+
+}
